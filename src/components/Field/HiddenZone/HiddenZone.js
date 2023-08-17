0aaa6599b0ec3ef2411629c1349875129299a414
AlexeySitnikov <alexey.sitnikov@bk.ru>
2023-08-17 15:18:21 +0300

    made a visual part of the field

--- src/components/Field/HiddenZone/HiddenZone.js
+++ src/components/Field/HiddenZone/HiddenZone.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-nested-ternary */
-// import mineRed from '../../constrains/mineRed.jpg'
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import { useState } from 'react'
 import style from './style.module.css'
+import mine from '../../constrains/mineRed.jpg'
 
 export function HiddenZone({ el }) {
   const [hidden, setHidden] = useState(true)
@@ -16,16 +18,39 @@ export function HiddenZone({ el }) {
   }
 
   return (
-    <div className={`${style.hiddenZone}`}>
-      <input
-        className={(hidden)
-          ? `${style.hidden}`
-          : `${style.unhidden}`}
-        type="button"
+    <div className={`${style.container}`}>
+
+      <div
+        className={`${style.square}`}
         onMouseDown={onClickFieldHandler}
-        onContextMenu={(e) => (e.preventDefault())}
-        value={(el === 'mine') ? 'm' : el}
-      />
+        onContextMenu={(e) => e.preventDefault()}
+      >
+        {(el === 'mine')
+          ? <img className={`${style.mine}`} src={`${mine}`} alt="mine" />
+          : (
+            <p className={(el === 1)
+              ? `${style.digit_1}`
+              : (el === 2)
+                ? `${style.digit_2}`
+                : (el === 3)
+                  ? `${style.digit_3}`
+                  : (el === 4)
+                    ? `${style.digit_4}`
+                    : (el === 5)
+                      ? `${style.digit_5}`
+                      : (el === 6)
+                        ? `${style.digit_6}`
+                        : (el === 7)
+                          ? `${style.digit_7}`
+                          : (el === 8) ? `${style.digit_8}` : `${style.digit}`}
+            >
+              {(el === 0) ? '' : el }
+            </p>
+          )}
+
+        <div className={(hidden) ? `${style.hideSquare}` : `${style.unhideSquare}`} />
+
+      </div>
     </div>
   )
 }
